318c8aad5716494af2f30d2dcb3d51a53070b9c1
sha3loul <zarhloulyasser@gmail.com>
2023-10-08 22:29:11 +0100

    feat: more tests, all passed

--- src/linkedlist/test/LinkedListTest.java
+++ src/linkedlist/test/LinkedListTest.java
@@ -16,6 +16,13 @@ public class LinkedListTest {
     }
 
     @Test
+    public void testIsEmpty() {
+        assertTrue(list.isEmpty());
+        list.add(5);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void testAddToEmptyList() {
         list.add(5);
         Cell head = list.findCell(5);
@@ -40,6 +47,54 @@ public class LinkedListTest {
         assertEquals(5, first.value);
         assertEquals(10, second.value);
         assertEquals(15, third.value);
+    }
+
+    @Test
+    public void testAddAtBeginning() {
+        list.add(10);
+        list.addAtBeginning(new Cell(5));
+        assertEquals(5, list.getValueAt(0));
+        assertEquals(10, list.getValueAt(1));
+    }
+
+    @Test
+    public void testAddAtEnd() {
+        list.add(5);
+        list.addAtEnd(new Cell(10));
+        assertEquals(5, list.getValueAt(0));
+        assertEquals(10, list.getValueAt(1));
+    }
+
+    @Test
+    public void testInsertAfter() {
+        list.add(5);
+        list.add(15);
+        Cell cell5 = list.findCell(5);
+        list.insertAfter(cell5, 10);
+        assertEquals(5, list.getValueAt(0));
+        assertEquals(10, list.getValueAt(1));
+        assertEquals(15, list.getValueAt(2));
+    }
+
+    @Test
+    public void testDeleteCell() {
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        Cell cell5 = list.findCellBefore(10);
+        list.deleteCell(cell5);
+        assertNull(list.findCell(10));
+        assertEquals(2, list.getSize());
+    }
+
+    @Test
+    public void testInsertCellToSorted() {
+        list.add(5);
+        list.add(15);
+        list.insertCellToSorted(new Cell(10));
+        assertEquals(5, list.getValueAt(0));
+        assertEquals(10, list.getValueAt(1));
+        assertEquals(15, list.getValueAt(2));
     }
 
     @Test
